d4c2981d43f3c5805bbf4ddfa6de2ad285df9247
MaxPavliv <maxyxpavliv@gmail.com>
2023-07-22 15:15:48 +0300

    add dame logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,47 @@
 package connectfour
-
+enum class pplayer(var name1:String,val sign:Char){
+    p1("p1",'o'),
+    p2("p2",'*')
+}
+var board1 = mutableListOf(
+    mutableListOf<Char>('A','A','A','A','A','A','A','A','A',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+    mutableListOf<Char>(' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',),
+)
+fun turn(n :Int):pplayer{
+    if(n%2==0)return pplayer.p1
+    else return pplayer.p2
+}
 class Connect_Four {
-    var p1 = ""
-    var p2 = ""
     var size = ""
     var W = 0
     var H = 0
+    var board = mutableListOf(mutableListOf<Char>(' '),)
+    var n1 = mutableListOf<Int>()
     init{
         start()
         inputSize()
         start_Info()
-        printboard()
+        createBoard()
+        temporPrint()
+        gameLogic()
+        println("Game over!")
     }
     fun start() {
         println("Connect Four")
         println("First player's name:")
-        p1 = readln()
+        pplayer.p1.name1 = readln()
         println("Second player's name:")
-        p2 = readln()
+        pplayer.p2.name1 = readln()
     }
     fun inputSize() {
         input@ while (true) {
@@ -49,36 +73,67 @@ class Connect_Four {
         }
     }
     fun start_Info() {
-        println(p1 + " vs " + p2)
+        println(pplayer.p1.name1 + " vs " + pplayer.p2.name1)
         println("$H X " + W + " board")
     }
+    fun createBoard(){
+        val a1 = mutableListOf<Char>()
+        for ( a  in 0..W){
+            a1.add(a,'E')
+        }
+        for ( a  in 0..W){
+            board.add(a,a1)
+            n1.add(a,H)
+        }
+    }
+    fun gameLogic(){
+        var n =0
+        game@ while( true){
 
-    fun printboard(){
+            println("${turn(n).name1}'s turn:")
+            var putLine = readln()
+            if(putLine=="end"){
+                break@game
+            }
+            if(!"\\d+".toRegex().matches(putLine)){
+                println("Incorrect column number")
+                continue@game}
+            if(putLine.isNullOrEmpty())continue@game
+            if ( !(putLine.toInt() in 1..W)){
+                println("The column number is out of range (1 - $W)")
+                continue@game
+            }
+            if (n1[putLine.toInt()]==0){
+                println( "Column $putLine is full")
+                continue@game
+            }
+            board1[putLine.toInt()][n1[putLine.toInt()]]=turn(n).sign
+            n1[putLine.toInt()]--
+
+            n++
+            temporPrint()
+        }
+    }
+    fun temporPrint(){
         for(b in 1..W){
             print(" $b")
         }
-        println("")
-
-        for(a in 1..H){
+        println()
+        for ( a in  1..H){
 
             for(b in 1..W){
-                print("║ ")
+                print("║${board1[b][a]}")
             }
-
             println("║")
-
+            // println()
         }
         print("╚")
-
         for(b in 1..W-1){
             print("═╩")
         }
-        print("═╝")
+        println("═╝")
     }
 }
-
 fun main() {
-
-    // println('║')
     val game = Connect_Four()
 }
